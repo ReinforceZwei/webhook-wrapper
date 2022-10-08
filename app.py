e42ef92bee28bee51bfd37b33e6b8ad72642c29d
ReinforceZwei <39955851+ReinforceZwei@users.noreply.github.com>
2022-10-08 20:08:54 +0800

    只差dashboard

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, redirect, render_template, request, jsonify, url_for
 import sqlite3
 import requests
 
@@ -6,8 +6,14 @@ DB_FILENAME = 'data.db'
 DB_SCHEMA = """
 create table if not exists "application" (
     "id" integer NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "name" text NOT NULL UNIQUE,
-    "webhook_url" text NOT NULL
+    "name" text NOT NULL,
+    "webhook_id" integer,
+    FOREIGN KEY ("webhook_id") REFERENCES "webhook" ("id") ON DELETE SET NULL
+);
+create table if not exists "webhook" (
+    "id" integer NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" text NOT NULL,
+    "url" text NOT NULL
 );
 """
 class ApiDatabase:
@@ -15,7 +21,7 @@ class ApiDatabase:
         self._db = sqlite3.connect(DB_FILENAME, check_same_thread=False)
         self._db.row_factory = sqlite3.Row
         self._cur = self._db.cursor()
-        self._cur.execute(DB_SCHEMA)
+        self._cur.executescript(DB_SCHEMA)
         self._db.commit()
     
     def _fetch_all(self):
@@ -26,65 +32,153 @@ class ApiDatabase:
         self._cur.execute(sql)
         return self._fetch_all()
     
-    def get_app(self, name):
-        sql = 'SELECT * FROM application WHERE name = ?'
-        self._cur.execute(sql, name)
+    def list_webhook(self):
+        sql = 'SELECT * FROM webhook'
+        self._cur.execute(sql)
+        return self._fetch_all()
+
+    def get_app(self, id):
+        sql = 'SELECT a.id as id,a.name as name,b.url as webhook_url FROM application as a JOIN webhook as b ON webhook_id = b.id WHERE a.id = ?'
+        self._cur.execute(sql, (id,))
         return self._cur.fetchone()
     
-    def add_app(self, name, webhook):
-        sql = 'INSERT INTO application(name,webhook_url) VALUES(?,?)'
-        self._cur.execute(sql, (name, webhook))
+    def get_webhook(self, id):
+        sql = 'SELECT * FROM webhook WHERE id = ?'
+        self._cur.execute(sql, (id,))
+        return self._cur.fetchone()
+    
+    def add_app(self, name, webhook_id) -> int:
+        sql = 'INSERT INTO application(name,webhook_id) VALUES(?,?)'
+        self._cur.execute(sql, (name, webhook_id))
+        self._db.commit()
+        return self._cur.lastrowid
+    
+    def add_webhook(self, name, url) -> int:
+        sql = 'INSERT INTO webhook(name,url) VALUES(?,?)'
+        self._cur.execute(sql, (name, url))
+        self._db.commit()
+        return self._cur.lastrowid
+    
+    def delete_app(self, id):
+        sql = 'DELETE FROM application WHERE id = ?'
+        self._cur.execute(sql, (id,))
         self._db.commit()
         return
     
-    def delete_app(self, name):
-        sql = 'DELETE FROM application WHERE name = ?'
-        self._cur.execute(sql, name)
+    def delete_webhook(self, id):
+        sql = 'DELETE FROM webhook WHERE id = ?'
+        self._cur.execute(sql, (id,))
         self._db.commit()
         return
     
-    def update_app(self, name, webhook):
-        sql = 'UPDATE application SET webhook_url = ? WHERE name = ?'
-        self._cur.execute(sql, (webhook, name))
+    def update_app(self, id, name, webhook_id):
+        sql = 'UPDATE application SET webhook_id = ?, name = ? WHERE id = ?'
+        self._cur.execute(sql, (webhook_id, name, id))
+        self._db.commit()
+        return
+    
+    def update_webhook(self, id, name, url):
+        sql = 'UPDATE webhook SET url = ?, name = ? WHERE id = ?'
+        self._cur.execute(sql, (url, name, id))
         self._db.commit()
         return
 
 def post_webhook(url, name, content):
-    return
+    BODY_TEMPLATE = '{"username":"$name","content":"$content"}'
+    b = BODY_TEMPLATE.replace('$name', name.replace('"', '\\"')).replace('$content', content.replace('"', '\\"'))
+    r = requests.post(url, b, headers={'content-type': 'application/json'})
+    return r.status_code == 200
 
 app = Flask(__name__)
 db = ApiDatabase()
 
 @app.route("/")
 def index():
-    return "Will be control panel"
+    return redirect(url_for('dashboard'))
 
-@app.route("/list")
+@app.route("/dashboard")
+def dashboard():
+    a = db.list_app()
+    w = db.list_webhook()
+    return render_template('dashboard.html', app=a, webhook=w)
+
+@app.route("/app/list")
 def list_app():
     return jsonify(db.list_app())
 
-@app.route("/add", methods=['POST'])
+@app.route("/app/add", methods=['POST'])
 def add_app():
     name = request.values.get("name", "", type=str)
-    webhook = request.values.get("webhook", "", type=str)
-    if name and webhook:
-        db.add_app(name, webhook)
+    webhook_id = request.values.get("webhook_id", "", type=int)
+    if name and webhook_id:
+        if db.get_webhook(webhook_id):
+            id = db.add_app(name, webhook_id)
+            return str(id), 201
+        else:
+            return "Not Found", 404
+    else:
+        return "Bad Request", 400
+
+@app.route("/app/delete/<int:id>", methods=['DELETE'])
+def delete_app(id):
+    db.delete_app(id)
     return "OK"
 
-@app.route("/delete/<str:name>", methods=['DELETE'])
-def delete_app(name):
-    db.delete_app(name)
+@app.route("/app/update/<int:id>", methods=['POST', 'UPDATE'])
+def update_app(id):
+    name = request.values.get("name", "", type=str)
+    webhook_id = request.values.get("webhook_id", "", type=int)
+    if name and webhook_id:
+        db.update_app(id, name, webhook_id)
+        return "OK"
+    else:
+        return "Bad Request", 400
+
+@app.route("/webhook/list")
+def list_webhook():
+    return jsonify(db.list_webhook())
+
+@app.route("/webhook/add", methods=['POST'])
+def add_webhook():
+    name = request.values.get("name", "", type=str)
+    url = request.values.get("url", "", type=str)
+    if name and url:
+        id = db.add_webhook(name, url)
+        return str(id), 201
+    else:
+        return "Bad Request", 400
+
+@app.route("/webhook/delete/<int:id>")
+def delete_webhook(id):
+    db.delete_webhook(id)
     return "OK"
 
-@app.route("/send/<str:name>")
-def execute_webhook(name):
+@app.route("/webhook/update/<int:id>", methods=['POST', 'UPDATE'])
+def update_webhook(id):
+    name = request.values.get("name", "", type=str)
+    url = request.values.get("url", "", type=str)
+    if name and url:
+        db.update_webhook(id, name, url)
+        return "OK"
+    else:
+        return "Bad Request", 400
+
+@app.route("/send/<int:id>", methods=['GET', 'POST'])
+def execute_webhook(id):
     if request.method == 'GET':
         content = request.args.get('content', '', type=str)
     elif request.method == 'POST':
         content = request.values.get('content', '', type=str)
-    w = db.get_app(name)
+    w = db.get_app(id)
     if w:
-        url = w['webhook_url']
-        requests.post()
-
-app.run()
+        if w['webhook_url']:
+            url = w['webhook_url']
+            post_webhook(url, w['name'], content)
+            return "OK"
+        else:
+            return "Bad Webhook ID", 400
+    else:
+        return "Not Found", 404
+    
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
